6f69779b165873884b1abe39a58dd7edae3b9052
yashraj <yashrajb916@gmail.com>
2026-09-12 15:13:40 +0530

    Add tailwind for writing fast css

---
+++ src/componants/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./style.css"
+const Navbar = () => {
+  return (
+    <div className='w-full bg-blue-900 text-white'>
+      Home
+    </div>
+  )
+}
+
+export default Navbar
