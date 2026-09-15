636cc3ff5da72f84d42afbab2a0065fce36b7be5
yashraj <yashrajb916@gmail.com>
2026-09-15 13:06:13 +0530

    Make code more readable

--- src/componants/Latest.jsx
+++ src/componants/Latest.jsx
@@ -1,10 +1,11 @@
 import "./style.css"
-import React, { useEffect , useState } from 'react'
-import {useRef} from 'react'
-import { nanoid } from 'nanoid'
-
+import React, { useEffect , useState , useRef} from 'react'
+import { nanoid } from 'nanoid'//import for generating random keys
+ 
 const Latest = () => {
+    //add variable to an element
     let ref = useRef(null);
+    //scroll that choosen element after a special event
     const scroll = (scrollOffset) => {
         if (ref.current) {
             ref.current.scrollBy({
@@ -13,30 +14,38 @@ const Latest = () => {
             });
         }   
     };
+    //movie list to show latest Movies
     const [Movie,setMovie] = useState([]);
+    //render movie when rendering the page
     useEffect(()=>{
         async function getMovie(){
+            //fetch movies from json 
             const response = await fetch(`${import.meta.env.BASE_URL}/data/movies.json`);
-            const data = await response.json();
+            const data = await response.json();//convert data as normal list object
+            
+            //contains latest movies
             let lMovieList = [];
+             
             const movieAdded = 12;
             //show only last 10 movies
             for(let i= data.length-1  ;i > data.length-movieAdded+1;i--){
                 console.log(i);
                 lMovieList.push(data[i]);
             };
-            console.log(lMovieList);
+            //set movies for randering
             setMovie(lMovieList);
 
         }
+        //call the function imidiatly after rendering 
         getMovie();
     },[])
 
+    //create image element for every movie for render on the screen
     const Latest_movies = Movie.map((movie)=>{
         return <img key = {nanoid()} className={`rounded-2xl  w-30 h-50 object-cover shrink-0 `} src={movie.picture} alt="MoviePic" />
     })
-    console.log(Latest_movies)
 
+    //main page
   return (
     <div className="w-full p-3 flex items-center justify-around">
       <div className="relative flex flex-col w-full md:px-26 px-3  gap-1 ">
@@ -44,21 +53,24 @@ const Latest = () => {
         <div ref = {ref} className="mask-[linear-gradient(to_right,transparent_0%,white_3%_97%,transparent_100%)] w-full  [&::-webkit-scrollbar]:hidden overflow-x-auto flex flex-row gap-2 items-center justify-around p-1">
             {Latest_movies}
         </div>
+        {/* Left button which will scroll left after clicking */}
         <div onClick={()=>{
-            scroll(-100)
+            scroll(-150)
         }} className="p-1 absolute top-[50%] md:flex hidden left-25  w-min rounded-2xl bg-white">
             <svg xmlns="http://w3.org" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
             <polyline points="15 18 9 12 15 6"></polyline>
             </svg>
         </div>
+        {/* Right button to scroll right */}
         <button onClick={()=>{
-            scroll(100);
+            scroll(150);
 
         }} className="z-30 p-1 absolute top-[50%] md:flex hidden right-25 w-min rounded-2xl bg-white">
             <svg xmlns="http://w3.org" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                 <polyline points="9 18 15 12 9 6"></polyline>
             </svg>
         </button>
+
       </div>
     </div>
   )
