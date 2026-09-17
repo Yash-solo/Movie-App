77e26ad9b5bbd30bd38bc7a6590601a8f072c31d
yashraj <yashrajb916@gmail.com>
2026-09-17 16:37:00 +0530

    change words to camel case

--- src/componants/Latest.jsx
+++ src/componants/Latest.jsx
@@ -71,7 +71,7 @@ const Latest = () => {
         <div onClick={()=>{
             scroll(-150)
         }} className="p-1 absolute top-[50%] md:flex hidden left-25  w-min rounded-2xl bg-white">
-            <svg xmlns="http://w3.org" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+            <svg xmlns="http://w3.org" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
             <polyline points="15 18 9 12 15 6"></polyline>
             </svg>
         </div>
@@ -80,7 +80,7 @@ const Latest = () => {
             scroll(150);
 
         }} className="z-30 p-1 absolute top-[50%] md:flex hidden right-25 w-min rounded-2xl bg-white">
-            <svg xmlns="http://w3.org" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+            <svg xmlns="http://w3.org" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                 <polyline points="9 18 15 12 9 6"></polyline>
             </svg>
         </button>
