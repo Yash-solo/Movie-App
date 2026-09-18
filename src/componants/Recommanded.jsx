80ae57a886dcfc2ec4e58d90a909d0b9bbd48962
yashraj <yashrajb916@gmail.com>
2026-09-18 15:18:06 +0530

    Recommanded right left btn hide

--- src/componants/Recommanded.jsx
+++ src/componants/Recommanded.jsx
@@ -2,9 +2,26 @@ import "./style.css"
 import React, { useEffect,useRef, useState } from 'react'
 import { nanoid } from "nanoid";
 const Recommanded = () => {
+    //get categories
     const category= JSON.parse(localStorage.getItem("categories"));
     let ref = useRef(null);
+    //movies that will going to render
+    const [movies,setMovie] = useState([]);
+    const [showBtn,setShowBtn] = useState(true);
+    let showBtnLeft = useRef();
+    let showBtnRight = useRef();
+    //return if no recommandations
+    if(category===null){
+        return 
+    }
     
+    useEffect(()=>{
+        if(movies.length<7){
+            showBtnLeft.current.style.display = "none"
+            showBtnRight.current.style.display = "none";
+        }
+    },[])
+    //scroll left to right for seeing the movies
     const scroll = (scrollByNum) =>{
         if(ref.current){
             ref.current.scrollBy({
@@ -13,7 +30,7 @@ const Recommanded = () => {
             })
         }
     }
-    const [movies,setMovie] = useState([]);
+    //use this effect when you wants to give recommandations
     useEffect(()=>{
         async function getRecommand(){
             const response = await fetch(`${import.meta.env.BASE_URL}/data/movies.json`);
@@ -52,7 +69,7 @@ const Recommanded = () => {
         getRecommand();
     },[])
     
-    
+    //actual rendering
   return (
     <>
      <div className="relative w-full p-3 md:px-25 flex justify-around items-center ">
@@ -62,19 +79,19 @@ const Recommanded = () => {
                 {...movies}
             </div>
             {/* Left button which will scroll left after clicking */}
-            <div onClick={()=>{
+            <div ref={showBtnLeft} onClick={()=>{
                 scroll(-150)
-            }} className="p-1 absolute top-[50%] md:flex hidden left-25  w-min rounded-2xl bg-white">
-                <svg xmlns="http://w3.org" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+            }}  className="p-1 absolute top-[50%] md:flex hidden left-25  w-min rounded-2xl bg-white">
+                <svg  xmlns="http://w3.org" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                 <polyline points="15 18 9 12 15 6"></polyline>
                 </svg>
             </div>
             {/* Right button to scroll right */}
-            <button onClick={()=>{
+            <button ref={showBtnRight} onClick={()=>{
                 scroll(150);
 
-            }} className="z-30 p-1 absolute top-[50%] md:flex hidden right-25 w-min rounded-2xl bg-white">
-                <svg xmlns="http://w3.org" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"  strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+            }}  className="z-30 p-1 absolute top-[50%] md:flex hidden right-25 w-min rounded-2xl bg-white">
+                <svg   xmlns="http://w3.org" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"  strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                     <polyline points="9 18 15 12 9 6"></polyline>
                 </svg>
             </button>
