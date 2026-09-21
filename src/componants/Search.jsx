171bce46672dc61337dd02e7048c9928bdaf8cef
yashraj <yashrajb916@gmail.com>
2026-09-21 15:05:33 +0530

    Choose the movies that will render after search:

--- src/componants/Search.jsx
+++ src/componants/Search.jsx
@@ -1,7 +1,49 @@
 import "./style.css"
-import React from 'react'
-
+import React, { useEffect } from 'react'
+import { useState , useRef } from "react"
 const SearchBar = () => {
+  //make variable using destructuring
+  const [searchMovie,setSearchMovie] = useState(null);//variable that store search value
+  const [SearchList,setSearchList] = useState([]);//variable that stores searchMovies
+
+  //please set a new search list that contains the movies that will come after search 
+  useEffect(()=>{
+    if(searchMovie === ""){
+      console.log("No Search");
+    }else{
+      async function getSearch(){
+        const response = await fetch(`${import.meta.env.BASE_URL}/data/movies.json`);
+        
+        if(response.ok){
+          const data = await response.json();
+          const newSearchList = data.filter((movie)=>{
+            return movie.MovieName.toLowerCase().includes(searchMovie);
+          })
+          
+          setSearchList(newSearchList);
+        }
+      }
+      getSearch();
+    }
+  },[searchMovie])
+
+
+  //debounce function that delays the function exicution of the handleChange 
+  const handleChange = Debounce((e)=>{
+    setSearchMovie(e.target.value);
+  },2000)
+  //debouce function blueprint
+  function Debounce(fn,delay){
+    let timer;
+    return function(...args){
+      clearTimeout(timer);
+      timer = setTimeout(()=>{
+        fn(...args)
+      },delay)
+    }
+  }
+
+
   return (
     <div className="w-full p-2 items-center justify-around flex">
       <div className="flex text-white gap-4 border border-[#c2c2c2] bg-gray-950 px-7 p-2">
@@ -9,7 +51,9 @@ const SearchBar = () => {
             <circle cx="11" cy="11" r="8"></circle>
             <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
         </svg>
-        <input className="focus:outline-0 w-full" type="text" placeholder="Search movie..."/>
+        <input className="focus:outline-0 w-full" onChange={(e)=>{
+          handleChange(e);
+        }} type="text" placeholder="Search movie..."/>
       </div>
     </div>
   )
