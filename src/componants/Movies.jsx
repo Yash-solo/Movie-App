48b5193d71440b09b7776a6bac267a6441fb21e1
yashraj <yashrajb916@gmail.com>
2026-09-21 16:01:35 +0530

    test commit

--- src/componants/Movies.jsx
+++ src/componants/Movies.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Movie = () => {
   return (
-    <div className=' flex flex-col items-center justify-around w-full bg-amber-300 p-2'>
+    <div className=' flex flex-col items-center justify-around w-full bg-amber-400 p-2'>
       <h1 className='text-2xl text-white font-bold font-serif w-full text-center'>Here is your  Movies</h1>
     </div>
   )
--- src/componants/Recommanded.jsx
+++ src/componants/Recommanded.jsx
@@ -80,7 +80,7 @@ const Recommanded = () => {
             </div>
             {/* Left button which will scroll left after clicking */}
             <div ref={showBtnLeft} onClick={()=>{
-                scroll(-150)
+                scroll(-500)
             }}  className="p-1 absolute top-[50%] md:flex hidden left-25  w-min rounded-2xl bg-white">
                 <svg  xmlns="http://w3.org" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                 <polyline points="15 18 9 12 15 6"></polyline>
@@ -88,7 +88,7 @@ const Recommanded = () => {
             </div>
             {/* Right button to scroll right */}
             <button ref={showBtnRight} onClick={()=>{
-                scroll(150);
+                scroll(500);
 
             }}  className="z-30 p-1 absolute top-[50%] md:flex hidden right-25 w-min rounded-2xl bg-white">
                 <svg   xmlns="http://w3.org" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"  strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
