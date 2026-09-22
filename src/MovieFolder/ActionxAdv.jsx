eb92cc90b52584c0397f71cd4ea454c1b1ea4c9e
yashraj <yashrajb916@gmail.com>
2026-09-22 15:45:00 +0530

    Add ActionXAdventure section to the Movie Section

--- src/MovieFolder/ActionxAdv.jsx
+++ src/MovieFolder/ActionxAdv.jsx
@@ -1,7 +1,11 @@
-import React from 'react'
-import { useRef } from 'react'
+
+import { useRef , useState , useEffect} from 'react'
+
 const ActionxAdv = () => {
+    //movie list that stores fetched data
+    const [moviesList,setMoviesList] = useState([]);
     const ref = useRef(null);
+    //scroll template
     const scroll =(scrollByNum)=>{
         if(ref.current){
             ref.current.scrollBy({
@@ -10,24 +14,55 @@ const ActionxAdv = () => {
             })
         }
     }
+    useEffect(()=>{
+        //get action advanture movies
+        async function getMovie(){
+            const response = await fetch(`${import.meta.env.BASE_URL}/data/movies.json`);
+            const data = await response.json();
+            setMoviesList(data);
+        }
+        getMovie();
+    },[])
+    
+    //filtering movies from the data 
+    let updateList = moviesList.filter((movie)=>{
+        return movie.category.includes("action") || movie.category.includes("adventure");
+    })
+    //select only 10 movies
+    updateList = updateList.reverse().slice(0,10);
+    //choosing only movies that should rander
+    const renderUpdate = updateList.map((movie)=>{
+        return <img onClick = {()=>{
+                    const getKey = JSON.parse(localStorage.getItem("watching"));
+                    localStorage.setItem("categories",JSON.stringify(movie.category));
+                    //open link where you can see your movie in 0 payment
+                    if(movie.path!=="xyz"){
+                        if(getKey!==null){
+                            localStorage.setItem("watching",JSON.stringify([...getKey,movie.MovieName]));
+                        }else{
+                            localStorage.setItem("watching",JSON.stringify([movie.MovieName]));
+                        }
+                        window.open(movie.path,"_blank")
+                        
+                    }else{
+                        alert("Movie not found");
+                    }
+                }} className="h-50 rounded-2xl shrink-0" src={movie.picture} alt="Movie" />
+        })
+
   return (
     <>
         <div className='relative w-full p-3 md:px-25 flex flex-col items-center justify-around gap-2'>
             <div className=' w-full flex flex-col gap-2'>
+                
+                {/* Here your movies will render */}
                 <h1 className='text-lg md:text-2xl lg:text-4xl text-white'>Action & Adventure Movies</h1>
                 <div ref={ref} className='overflow-x-auto p-2 w-full [&::-webkit-scrollbar]:hidden mask-[linear-gradient(to_right,transparent_0%,black_10%_90%,transparent_100%)] flex flex-row gap-7 items-center justify-around'>
-                    <img className="h-50 rounded-2xl shrink-0"src="./photos/hanuman ansh.jpg" alt="" />
-                    <img className="h-50 rounded-2xl shrink-0"src="./photos/hanuman ansh.jpg" alt="" />
-                    <img className="h-50 rounded-2xl shrink-0"src="./photos/hanuman ansh.jpg" alt="" />
-                    <img className="h-50 rounded-2xl shrink-0"src="./photos/hanuman ansh.jpg" alt="" />
-                    <img className="h-50 rounded-2xl shrink-0"src="./photos/hanuman ansh.jpg" alt="" />
-                    <img className="h-50 rounded-2xl shrink-0"src="./photos/hanuman ansh.jpg" alt="" />
-                    <img className="h-50 rounded-2xl shrink-0"src="./photos/hanuman ansh.jpg" alt="" />
-                    <img className="h-50 rounded-2xl shrink-0"src="./photos/hanuman ansh.jpg" alt="" />
-                    <img className="h-50 rounded-2xl shrink-0"src="./photos/hanuman ansh.jpg" alt="" />
-                    <img className="h-50 rounded-2xl shrink-0"src="./photos/hanuman ansh.jpg" alt="" />
-                    <img className="h-50 rounded-2xl shrink-0"src="./photos/hanuman ansh.jpg" alt="" />
+                    {renderUpdate}
                 </div>
+
+
+                {/* left button to scroll left */}
                 <button  onClick={()=>{
                     scroll(-550)
                 }} className="p-1 absolute top-[50%] md:flex hidden left-25  w-min rounded-2xl bg-white">
--- src/MovieFolder/Top10.jsx
+++ src/MovieFolder/Top10.jsx
@@ -1,9 +1,11 @@
-import { img } from 'framer-motion/client';
-import React, { useEffect,useState } from 'react'
-import { useRef } from 'react'
+import { useEffect,useState,useRef} from 'react'
+//main top function 
 const Top10 = () => {
+
+    //store only top 10 movies
     const [topMovies,setTopMovies] = useState([]);
     const ref = useRef(null);
+    //scroll template
     const scroll = (scrollNum)=>{
         if(ref.current){
             ref.current.scrollBy({
@@ -15,31 +17,33 @@ const Top10 = () => {
 
     useEffect(()=>{
         async function getTopMovies(){
+            //fetch data from anywhere
             const response = await fetch(`${import.meta.env.BASE_URL}/data/TopMovies.json`)
             const data = await response.json();
+            //top rendering 
             const TopMovies = data.map((movie)=>{
                 return (
-                    <>
-                        <div className='relative shrink-0'>
-                            <h1 className='absolute bottom-0 -left-6 text-8xl text-[rgb(17,17,17)] font-bold [-webkit-text-stroke:1px_#ddd]'>{movie.id+1}</h1>
-                            <img onClick = {()=>{
-                                const getKey = JSON.parse(localStorage.getItem("watching"));
-                                localStorage.setItem("categories",JSON.stringify(movie.category));
-                                //open link where you can see your movie in 0 payment
-                                if(movie.path!=="xyz"){
-                                    if(getKey!==null){
-                                        localStorage.setItem("watching",JSON.stringify([...getKey,movie.MovieName]));
-                                    }else{
-                                        localStorage.setItem("watching",JSON.stringify([movie.MovieName]));
-                                    }
-                                    window.open(movie.path,"_blank")
-                                    
+                    
+                    <div key={movie.id} className='relative shrink-0'>
+                        <h1 className='absolute bottom-0 -left-6 text-8xl text-[rgb(17,17,17)] font-bold [-webkit-text-stroke:1px_#ddd]'>{movie.id+1}</h1>
+                        <img onClick = {()=>{
+                            const getKey = JSON.parse(localStorage.getItem("watching"));
+                            localStorage.setItem("categories",JSON.stringify(movie.category));
+                            //open link where you can see your movie in 0 payment
+                            if(movie.path!=="xyz"){
+                                if(getKey!==null){
+                                    localStorage.setItem("watching",JSON.stringify([...getKey,movie.MovieName]));
                                 }else{
-                                    alert("Movie not found");
+                                    localStorage.setItem("watching",JSON.stringify([movie.MovieName]));
                                 }
-                            }} className='h-50   rounded-2xl cursor-pointer' src={movie.picture} alt="MovieName" />
-                        </div>
-                    </>
+                                window.open(movie.path,"_blank")
+                                
+                            }else{
+                                alert("Movie not found");
+                            }
+                        }} className='h-50   rounded-2xl cursor-pointer' src={movie.picture} alt="MovieName" />
+                    </div>
+                    
                 )
             })
             setTopMovies(TopMovies);
@@ -55,7 +59,7 @@ const Top10 = () => {
       <div ref={ref} className='relative [&::-webkit-scrollbar]:hidden overflow-x-auto flex mask-[linear-gradient(to_right,transparent_0%,black_10%_90%,transparent_100%)] flex-row items-center gap-10 w-full   p-5'>
         {topMovies}
       </div>
-      {/* Left button which will scroll left after clicking */}
+            {/* Left button which will scroll left after clicking */}
             <div  onClick={()=>{
                 scroll(-550)
             }} className="p-1 absolute top-[50%] md:flex hidden left-25  w-min rounded-2xl bg-white">
