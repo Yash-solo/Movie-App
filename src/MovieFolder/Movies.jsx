5ba2e74809227549d306f80fa9d9ae6ff0824457
yashraj <yashrajb916@gmail.com>
2026-09-23 12:14:00 +0530

    Merge the Top10 movies for the Movies.jsx

--- src/MovieFolder/Movies.jsx
+++ src/MovieFolder/Movies.jsx
@@ -1,18 +1,81 @@
 import React from 'react'
 import About from '../componants/About.jsx'
 import HeroSection from '../componants/HeroSection'
-import { motion } from 'framer-motion'
+import { motion, useReducedMotion, useScroll } from 'framer-motion'
 import Top10 from "./Top10.jsx"
 import ActionxAdv from './ActionxAdv.jsx'
 import SouthSec from './SouthSec.jsx'
+import { nanoid } from 'nanoid'
+import { useRef , useEffect ,useState } from 'react'
 const Movie = () => {
+  const ref = useRef(null);
+  const [topMovies,setTopMovies] = useState([]);
+  const scroll = (scrollByNum)=>{
+    if(ref.current){
+      ref.current.scrollBy({
+        left:scrollByNum,
+        behavior:"smooth"
+      })
+    }
+  }
+  useEffect(()=>{
+    async function getTopMovies(){
+        //fetch data from anywhere
+        const response = await fetch(`${import.meta.env.BASE_URL}/data/TopMovies.json`)
+        const data = await response.json();
+        setTopMovies(data);
+    }
+    getTopMovies();
+  },[])
+
+  const TopMoviesNet = topMovies.map((movie)=>{
+    return <Top10 onClick={()=>{
+      const getKey = JSON.parse(localStorage.getItem("watching"));
+      localStorage.setItem("categories",JSON.stringify(movie.category));
+      //open link where you can see your movie in 0 payment
+      if(movie.path!=="xyz"){
+          if(getKey!==null){
+              localStorage.setItem("watching",JSON.stringify([...getKey,movie.MovieName]));
+          }else{
+              localStorage.setItem("watching",JSON.stringify([movie.MovieName]));
+          }
+          window.open(movie.path,"_blank")
+          
+      }else{
+          alert("Movie not found");
+      }      
+    }} key={movie.id} picture={movie.picture} id={movie.id} MovieName={movie.MovieName}/>
+  })
+
   return (
     <motion.div initial={{opacity:0}}animate={{opacity:1}}exit={{opacity:0}}>
-      <HeroSection aboutMovie = " Set in a fictional village of Dakshina Kannada, the story centers around a human-versus-nature conflict and a multi-generational land dispute"heroMovie="./photos/heroOf Movie.png" path="https://www.youtube.com/watch?v=x6Xemdjqrlw"MovieName = "KANTARA"/>
+      <HeroSection aboutMovie = "Set in a fictional village of Dakshina Kannada, the story centers around a human-versus-nature conflict and a multi-generational land dispute"heroMovie="./photos/heroOf Movie.png" path="https://www.youtube.com/watch?v=x6Xemdjqrlw"MovieName = "KANTARA"/>
       <div className='w-full p-3 text-center flex flex-col gap-5 items-center justify-around'>
         <h1 className='text-3xl text-[#ddd]'>See your Favorite Movies For Free From Any where</h1>
       </div>
-      <Top10/>
+      <div key={nanoid()} className='relative w-full flex-col  md:px-25 p-3 flex  '>
+          <h1 className='text-lg md:text-2xl lg:text-3xl text-white'>Top 10 Movies on Netflix</h1>
+          <div ref={ref} className='relative [&::-webkit-scrollbar]:hidden overflow-x-auto flex mask-[linear-gradient(to_right,transparent_0%,black_10%_90%,transparent_100%)] flex-row items-center gap-10 w-full   p-5'>
+              {TopMoviesNet}
+          </div>
+          {/* Left button which will scroll left after clicking */}
+          <button  onClick={()=>{
+              scroll(-550)
+          }} className="p-1 absolute top-[50%] md:flex hidden left-25  w-min rounded-2xl bg-white">
+              <svg xmlns="http://w3.org" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+              <polyline points="15 18 9 12 15 6"></polyline>
+              </svg>
+          </button>
+          {/* Right button to scroll right */}
+          <button   onClick={()=>{
+              scroll(550);
+      
+          }}  className="z-30 p-1 absolute top-[50%] md:flex hidden right-25 w-min rounded-2xl bg-white">
+              <svg  xmlns="http://w3.org" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                  <polyline points="9 18 15 12 9 6"></polyline>
+              </svg>
+          </button>
+      </div>
       <ActionxAdv/>
       <SouthSec/>
       <About/>
--- src/MovieFolder/Top10.jsx
+++ src/MovieFolder/Top10.jsx
@@ -1,58 +1,12 @@
 import { nanoid } from 'nanoid';
 import { useEffect,useState,useRef} from 'react'
 //main top function 
-const Top10 = () => {
+const Top10 = (props) => {
     const [isopen,setopen] = useState(false);
-    //store only top 10 movies
-    const [topMovies,setTopMovies] = useState([]);
-    const ref = useRef(null);
-    //scroll template
-    const scroll = (scrollNum)=>{
-        if(ref.current){
-            ref.current.scrollBy({
-                left:scrollNum,
-                behavior:"smooth"
-            })
-        }
-    }
-
-    useEffect(()=>{
-        async function getTopMovies(){
-            //fetch data from anywhere
-            const response = await fetch(`${import.meta.env.BASE_URL}/data/TopMovies.json`)
-            const data = await response.json();
-            //top rendering 
-            const TopMovies = data.map((movie)=>{
-                return (
-                    
-                    <div key={movie.id} className='relative shrink-0'>
-                        <h1 className='absolute bottom-0 -left-6 text-8xl text-[rgb(17,17,17)] font-bold [-webkit-text-stroke:1px_#ddd]'>{movie.id+1}</h1>
-                        <img  onClick = {()=>{
-                            setopen(true);
-                            // const getKey = JSON.parse(localStorage.getItem("watching"));
-                            // localStorage.setItem("categories",JSON.stringify(movie.category));
-                            // //open link where you can see your movie in 0 payment
-                            // if(movie.path!=="xyz"){
-                            //     if(getKey!==null){
-                            //         localStorage.setItem("watching",JSON.stringify([...getKey,movie.MovieName]));
-                            //     }else{
-                            //         localStorage.setItem("watching",JSON.stringify([movie.MovieName]));
-                            //     }
-                            //     window.open(movie.path,"_blank")
-                                
-                            // }else{
-                            //     alert("Movie not found");
-                            // }
-                        }} className='h-50   rounded-2xl cursor-pointer' src={movie.picture} alt="MovieName" />
-                    </div>
-                    
-                )
-            })
-            setTopMovies(TopMovies);
-        }
-        getTopMovies();
-    },[])
     
+    function handleChange(){
+        setopen(!isopen);
+    }
     const FakeTemplate = [(
         <>
         <div className='h-full w-full top-0 z-1000 p-2 backdrop-blur-[10px] bg-[rgba(17,17,17,0.3)] fixed  flex items-center justify-center'>
@@ -62,9 +16,9 @@ const Top10 = () => {
                     <span className='h-0.5 w-6 rotate-45 -translate-y-0.5 translate-x-0.2 bg-[rgba(255,255,255,0.5)]'></span>
                 </button>
                 <div className='flex flex-row gap-7'>
-                    <img className="h-50 md:h-70 rounded-2xl" src="./photos/hanuman ansh.jpg" alt="" />
+                    <img className="h-50 md:h-70 rounded-2xl" src={props.picture} alt="" />
                     <div className='flex flex-col gap-2'>
-                        <h1 className='text-[#ddd] font-semibold text-[20px] md:text-4xl'>Hanuman ansh</h1>
+                        <h1 className='text-[#ddd] font-semibold text-[20px] md:text-4xl'>{props.MovieName}</h1>
                         <h2 className='text-[#ddd] md:text-[25px]'>⭐8.2 rating</h2>
                         <h2 className='text-[#ddd] text-[17px] md:text-[20px]'>Category</h2>
                         <ul className='px-5 text-gray-300  md:text-[20px] list-decimal'>
@@ -76,37 +30,20 @@ const Top10 = () => {
                     </div>
                 </div>
                 <div className='w-full py-2 md:py-5 flex flex-col md:flex-row items-center gap-2 justify-around'>
-                    <button className='text-lg bg-red-500 px-3 p-2 w-full rounded-lg font-bold text-[#ddd]'>Watch Now</button>
-                    <button className='text-lg border border-[#ddd] px-3 p-2 w-full rounded-lg font-bold text-[#ddd]' >Add To Watch Later</button>
+                    <button className='text-lg bg-red-500 px-3 cursor-pointer p-2 w-full rounded-lg font-bold text-[#ddd]'>Watch Now</button>
+                    <button className='text-lg border border-[#ddd] cursor-pointer px-3 p-2 w-full rounded-lg font-bold text-[#ddd]' >Add To Watch Later</button>
                 </div>
             </div>
         </div>
         </>
     )]
     const realTemplate = [(
-        <div key={nanoid()} className='relative w-full flex-col  md:px-25 p-3 flex  '>
-            <h1 className='text-lg md:text-2xl lg:text-3xl text-white'>Top 10 Movies on Netflix</h1>
-            <div ref={ref} className='relative [&::-webkit-scrollbar]:hidden overflow-x-auto flex mask-[linear-gradient(to_right,transparent_0%,black_10%_90%,transparent_100%)] flex-row items-center gap-10 w-full   p-5'>
-                {topMovies}
+        <>
+            <div onClick={()=>handleChange()}key={props.id+1} className='relative shrink-0'>
+                <h1 className='absolute bottom-0 -left-6 text-8xl text-[rgb(17,17,17)] font-bold [-webkit-text-stroke:1px_#ddd]'>{props.id+1}</h1>
+                <img className='h-50   rounded-2xl cursor-pointer' src={props.picture} alt={props.MovieName} />
             </div>
-            {/* Left button which will scroll left after clicking */}
-            <button  onClick={()=>{
-                scroll(-550)
-            }} className="p-1 absolute top-[50%] md:flex hidden left-25  w-min rounded-2xl bg-white">
-                <svg xmlns="http://w3.org" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-                <polyline points="15 18 9 12 15 6"></polyline>
-                </svg>
-            </button>
-            {/* Right button to scroll right */}
-            <button   onClick={()=>{
-                scroll(550);
-
-            }}  className="z-30 p-1 absolute top-[50%] md:flex hidden right-25 w-min rounded-2xl bg-white">
-                <svg  xmlns="http://w3.org" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-                    <polyline points="9 18 15 12 9 6"></polyline>
-                </svg>
-            </button>
-        </div>
+        </>
     )]
 
   return (
