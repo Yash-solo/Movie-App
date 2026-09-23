13eba9a98d7278bb7d5059f85c6fffa929e8cdca
yashraj <yashrajb916@gmail.com>
2026-09-23 14:11:52 +0530

    Solved Categories problem

--- src/MovieFolder/Movies.jsx
+++ src/MovieFolder/Movies.jsx
@@ -43,7 +43,7 @@ const Movie = () => {
       }else{
           alert("Movie not found");
       }      
-    }} key={movie.id} picture={movie.picture} id={movie.id} MovieName={movie.MovieName}/>
+    }} path={movie.path} key={movie.id} picture={movie.picture} id={movie.id} MovieName={movie.MovieName}/>
   })
 
   return (
--- src/MovieFolder/Top10.jsx
+++ src/MovieFolder/Top10.jsx
@@ -10,12 +10,12 @@ const Top10 = (props) => {
     const FakeTemplate = [(
         <>
         <div className='h-full w-full top-0 left-0 z-1000 p-2 backdrop-blur-[10px] bg-[rgba(17,17,17,0.3)] fixed  flex items-center justify-center'>
-            <div className='p-3 px-6 shadow-[0px_0px_20px_rgba(255,255,255,0.2)] rounded-2xl border border-[#535252] flex-col w-3/4 bg-[rgba(17,17,17)] fixed  flex items-start justify-start'>
+            <div className='p-3 px-6 shadow-[0px_0px_20px_rgba(255,255,255,0.2)] rounded-2xl border border-[#535252] flex-col w-9/10 md:w-1/2 bg-[rgba(17,17,17)] fixed  flex items-start justify-start'>
                 <button onClick={()=>setopen(false)} className='cursor-pointer flex py-3 flex-col gap-2 items-end justify-end w-full text-end text-2xl font-mono text-white font-bold '>
                     <span className='h-0.5 -rotate-45 translate-y-2 w-6 bg-[rgba(255,255,255,0.5)]'></span>
                     <span className='h-0.5 w-6 rotate-45 -translate-y-0.5 translate-x-0.2 bg-[rgba(255,255,255,0.5)]'></span>
                 </button>
-                <div className='flex flex-row gap-5'>
+                <div className='flex flex-row gap-5 md:gap-7'>
                     <img className="h-50 md:h-70 rounded-2xl" src={props.picture} alt="" />
                     <div className='flex flex-col gap-2'>
                         <h1 className='text-[#ddd] font-semibold text-[20px] md:text-4xl'>{props.MovieName}</h1>
@@ -30,7 +30,13 @@ const Top10 = (props) => {
                     </div>
                 </div>
                 <div className='w-full py-2 md:py-5 flex flex-col md:flex-row items-center gap-2 justify-around'>
-                    <button className='text-lg bg-red-500 px-3 cursor-pointer p-2 w-full rounded-lg font-bold text-[#ddd]'>Watch Now</button>
+                    <button onClick={()=>{
+                        if(props.path!=="xyz"){
+                            window.open(props.path,"_blank")
+                        }else{
+                            alert("Movie Not Found")
+                        }
+                    }}className='text-lg bg-red-500 px-3 cursor-pointer p-2 w-full rounded-lg font-bold text-[#ddd]'>Watch Now</button>
                     <button className='text-lg border border-[#ddd] cursor-pointer px-3 p-2 w-full rounded-lg font-bold text-[#ddd]' >Add To Watch Later</button>
                 </div>
             </div>
