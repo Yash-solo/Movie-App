373589b0f46cd23422f609e7f27040bbf7253bcc
yashraj <yashrajb916@gmail.com>
2026-09-26 12:42:52 +0530

    Add

--- src/SeriesFolder/TV.jsx
+++ src/SeriesFolder/TV.jsx
@@ -1,10 +1,12 @@
 import { motion } from 'framer-motion'
 import React from 'react'
 import HeroSection from '../componants/HeroSection'
+import "./Tv.css"
 const TV = () => {
   return (
     <motion.div initial={{opacity:0}}animate={{opacity:1}}exit={{opacity:0}}>
-       <HeroSection aboutMovie = 'The plot follows a brilliant, reclusive mastermind known as "The Professor" who recruits a team of eight skilled criminals to execute two incredibly complex, multi-day robberies' heroMovie="./SeriesPhoto/SeriesHero.png" path="https://www.youtube.com/watch?v=x6Xemdjqrlw"MovieName = "Money Heist"/>
+      <HeroSection aboutMovie = 'The plot follows a brilliant, reclusive mastermind known as "The Professor" who recruits a team of eight skilled criminals to execute two incredibly complex, multi-day robberies' heroMovie="./SeriesPhoto/SeriesHero.png" path="https://www.youtube.com/watch?v=x6Xemdjqrlw"MovieName = "Money Heist"/>
+      
     </motion.div>
   )
 }
